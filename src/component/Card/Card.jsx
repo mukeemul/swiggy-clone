1fa2f89f0ec32dc298af65de0694772232787927
Mukeemul Haque <mukeemul9@gmail.com>
2023-03-26 16:26:00 +0530

    Adding a Searchbar

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,58 +1,57 @@
-import React from 'react'
-// import './Card.css';
+import React, { useState } from 'react'
 import RepData from '../../config/data';
 import { CiStar } from 'react-icons/ci';
 
-const Card = ( props ) => {
+const Card = (props) => {
   return (
-      <div className="banner">
-          <img src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${props?.Data.image}`} alt="Swiggy"  />
-          <h3>{props?.Data?.name}</h3>
-          <p>{props?.Data?.cuisines.join( ", " )}</p>
-          <div className="rating">
-            <div className="rate"><CiStar />{props?.Data?.rating}</div>
-            <div className="time">{props?.Data?.time}</div>
-            <div className="price">{props?.Data?.cost}</div>
-          </div>
+    <div className="banner">
+      <img src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${props?.Data?.image}`} alt="Swiggy" />
+      <h3>{props?.Data?.name}</h3>
+      <p>{props?.Data?.cuisines.join(", ")}</p>
+      <div className="rating">
+        <div className="rate"><CiStar />{props?.Data?.rating}</div>
+        <div className="time">{props?.Data?.time}</div>
+        <div className="price">{props?.Data?.cost}</div>
       </div>
+    </div>
   );
 }
 
 const BodyApp = () => {
   return (
     <>
-      <Card Data = {RepData[0]}/>
-      <Card Data = {RepData[1]}/>
-      <Card Data = {RepData[2]}/>
-      <Card Data = {RepData[3]}/>
-      <Card Data = {RepData[4]}/>
-      <Card Data = {RepData[5]}/>
-      <Card Data = {RepData[6]}/>
-      <Card Data = {RepData[7]}/>
-      <Card Data = {RepData[8]}/>
-      <Card Data = {RepData[9]}/>
-      <Card Data = {RepData[10]}/>
-      <Card Data = {RepData[11]}/>
-      <Card Data = {RepData[12]}/>
-      <Card Data = {RepData[13]}/>
-      <Card Data = {RepData[14]}/>
-      <Card Data = {RepData[15]}/>
-      <Card Data = {RepData[16]}/>
-      <Card Data = {RepData[17]}/>
-      <Card Data = {RepData[18]}/>
-      <Card Data = {RepData[19]}/>
-      <Card Data = {RepData[20]}/>
-      <Card Data = {RepData[21]}/>
-      <Card Data = {RepData[22]}/>
-      <Card Data = {RepData[23]}/>
-      <Card Data = {RepData[24]}/>
-      <Card Data = {RepData[25]}/>
-      <Card Data = {RepData[26]}/>
-      <Card Data = {RepData[27]}/>
-      <Card Data = {RepData[28]}/>
-      <Card Data = {RepData[29]}/>
-      <Card Data = {RepData[30]}/>
-      <Card Data = {RepData[31]}/> 
+      <Card Data={RepData[0]} />
+      <Card Data={RepData[1]} />
+      <Card Data={RepData[2]} />
+      <Card Data={RepData[3]} />
+      <Card Data={RepData[4]} />
+      <Card Data={RepData[5]} />
+      <Card Data={RepData[6]} />
+      <Card Data={RepData[7]} />
+      <Card Data={RepData[8]} />
+      <Card Data={RepData[9]} />
+      <Card Data={RepData[10]} />
+      <Card Data={RepData[11]} />
+      <Card Data={RepData[12]} />
+      <Card Data={RepData[13]} />
+      <Card Data={RepData[14]} />
+      <Card Data={RepData[15]} />
+      <Card Data={RepData[16]} />
+      <Card Data={RepData[17]} />
+      <Card Data={RepData[18]} />
+      <Card Data={RepData[19]} />
+      <Card Data={RepData[20]} />
+      <Card Data={RepData[21]} />
+      <Card Data={RepData[22]} />
+      <Card Data={RepData[23]} />
+      <Card Data={RepData[24]} />
+      <Card Data={RepData[25]} />
+      <Card Data={RepData[26]} />
+      <Card Data={RepData[27]} />
+      <Card Data={RepData[28]} />
+      <Card Data={RepData[29]} />
+      <Card Data={RepData[30]} />
+      <Card Data={RepData[31]} />
     </>
   )
 }
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,31 +1,47 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../../Assets/logo.png'
-import { RiArrowDropDownLine, RiSearchLine } from 'react-icons/ri';
-import { MdLocalOffer, MdOutlineHelp, MdFilterList } from 'react-icons/md';
+import RepData from '../../config/data'
+import { RiArrowDropDownLine } from 'react-icons/ri';
+import { MdLocalOffer, MdOutlineHelp, MdFilterList, MdHome } from 'react-icons/md';
 import { CgProfile } from 'react-icons/cg';
 import { BsCartCheck } from 'react-icons/bs';
 
 const Navbar = () => {
+    const [data, setData] = useState(RepData);
+    const [title, setTitle] = useState('');
     return (
         <div className='container'>
             <header className='navbar'>
                 <div className='logo'>
                     <img src={logo} alt="Swiggy" />
                     <h4 className='heading'>Other</h4>
-                    <input type="search" placeholder='Neoria Husainpur' />
+                    <input id='search' type="search" placeholder='Neoria Husainpur' />
                     <RiArrowDropDownLine className='drop' />
                 </div>
                 <div className="nav-links">
                     <ul className='nav-items'>
-                        <li><RiSearchLine className='icons' /><a href="/">Search</a></li>
+                        <li><MdHome className='icons' /><a href="/">Home</a></li>
                         <li><MdLocalOffer className='icons1' /><a href="/">Offers</a></li>
                         <li><MdOutlineHelp className='icons2' /><a href="/">Help</a></li>
                         <li><CgProfile className='icons3' /><a href="/">Sign In</a></li>
                         <li><BsCartCheck className='icons4' /><a href="/">Cart</a></li>
                     </ul>
                 </div>
+                <div className="searchbar">
+                    <input className="search" placeholder='Type to search'
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <button onChange={ () =>{
+                        const filterData = RepData.filter( (items) =>{
+                            return items?.RepData?.name.includes(title);
+                        });
+                        setData(filterData);
+                    }}>Search</button>
+                </div>
             </header>
+
         </div>
     );
 }
